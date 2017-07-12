0126c7aede84bd9840ad79c15a91feaea15328c1
Itzli <itzli2000@msn.com>
2017-07-12 13:47:10 -0500

    Added comments to functions php

--- wp-content/themes/basetheme/functions.php
+++ wp-content/themes/basetheme/functions.php
@@ -2,14 +2,18 @@
 //Adding stylesheets
 add_action('wp_enqueue_scripts','_adding_stylesheet');
  function _adding_stylesheet(){
+ 	//Style register
  	wp_register_style('style',get_template_directory_uri().'/style.css',array(),'1.0 ');
+ 	//Style display
  	wp_enqueue_style('style');
  }
 
 //Action that call javascript file on themebase assets/js
 add_action( 'wp_enqueue_scripts', '_adding_scripts',100 ); 
 function _adding_scripts() {
+	//JS file regist
 	wp_register_script('my_custom_script', get_template_directory_uri() . '/assets/js/theme-scripts.js', array('jquery'),'1', true);
+	//Js load
 	wp_enqueue_script('my_custom_script');
 }
 
