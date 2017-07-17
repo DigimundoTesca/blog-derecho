cb9144c496777a601a496ecc672103fea57891d5
Ariel Itzli Molina Sanchez <itzli2000@msn.com>
2017-07-17 11:05:33 -0500

    Moddified images styles for 'About' page, added new way to implement projects, removed 'Apply online' section from job board

--- wp-content/themes/basetheme/functions.php
+++ wp-content/themes/basetheme/functions.php
@@ -45,16 +45,54 @@ add_action( 'wp_default_scripts', function( $scripts ) {
 } );
 
 //Expires cookie for "jobs" page when user close the page
-// function dn_expire_psw_session() {
-//     if ( isset( $_COOKIE['wp-postpass_' . COOKIEHASH] ) ) {
-//         setcookie('wp-postpass_' . COOKIEHASH, '', 0, COOKIEPATH);
-//     }
-// }
-// add_action( 'wp', 'dn_expire_psw_session' );
+function custom_password_cookie_expiry( $expires ) {
+    return (time()+1800);  // Make it a session cookie
+}
+add_filter( 'post_password_expires', 'custom_password_cookie_expiry' );
 
 //Removes the 'Protected' string from title pages
 function change_protected_title_prefix() {
     return '%s';
 }
 add_filter('protected_title_format', 'change_protected_title_prefix');
+
+//Projects Post type
+add_action( 'init', 'blog_projects' );
+function blog_projects() {
+  $labels = array(
+    'name'               => _x( 'Proyectos', 'blog' ),
+    'singular_name'      => _x( 'Proyecto', 'post type singular name', 'blog' ),
+    'menu_name'          => _x( 'Proyectos', 'admin menu', 'blog' ),
+    'name_admin_bar'     => _x( 'Proyectos', 'add new on admin bar', 'blog' ),
+    'add_new'            => _x( 'Agregar Nuevo', 'book', 'blog' ),
+    'add_new_item'       => __( 'Agregar Nuevo Proyecto', 'blog' ),
+    'new_item'           => __( 'Nuevo Proyecto', 'blog' ),
+    'edit_item'          => __( 'Editar Proyectos', 'blog' ),
+    'view_item'          => __( 'Ver Proyectos', 'blog' ),
+    'all_items'          => __( 'Todos los Proyectos', 'blog' ),
+    'search_items'       => __( 'Buscar Proyectos', 'blog' ),
+    'parent_item_colon'  => __( 'Parent Proyectos:', 'blog' ),
+    'not_found'          => __( 'No Projects found.', 'blog' ),
+    'not_found_in_trash' => __( 'No Projects found in Trash.', 'blog' )
+  );
+
+  $args = array(
+    'labels'             => $labels,
+    'description'        => __( 'Description.', 'blog' ),
+    'public'             => true,
+    'publicly_queryable' => true,
+    'show_ui'            => true,
+    'show_in_menu'       => true,
+    'query_var'          => true,
+    'rewrite'            => array( 'slug' => 'proyectosblog' ),
+    'capability_type'    => 'post',
+    'has_archive'        => true,
+    'hierarchical'       => false,
+    'menu_position'      => 6,
+    'supports'           => array( 'title', 'editor', 'thumbnail' ),
+    'taxonomies'          => array( 'category' ),
+  );
+
+  register_post_type( 'proyectosblog', $args );
+}
 ?>
--- wp-content/themes/basetheme/page-quienes-somos.php
+++ wp-content/themes/basetheme/page-quienes-somos.php
@@ -7,37 +7,56 @@
       <p class="mobiletext">
         ¡Da click en las imagenes!
       </p>
-      <div class="projectContainer">
+      <?php 
+      $args = array(
+        'post_type' => 'proyectosblog',
+        'posts_per_page' => 3,
+        'orderby' => 'title',
+        'order' => 'ASC'
+        );
+      $proye = new WP_Query($args);
+      while ($proye->have_posts()): $proye->the_post();
+      ?>
+      <div class="projectContainer truncate">
+      <div class="ih-item square effect6 from_top_and_bottom"><a href="#">
+        <div class="img"><img src="<?php the_post_thumbnail(); ?>" alt="Imagen de Proyecto"></div>
+        <div class="info">
+          <h3><?php the_title(); ?></h3>
+          <?php the_content(); ?>
+        </div></a></div>
+      </div>
+    <?php endwhile; wp_reset_postdata(); ?>
+    <div class="projectContainer truncate">
+      <div class="ih-item square effect6 from_top_and_bottom"><a href="#">
+        <div class="img"><img src="<?php the_field('imagen-proyecto_1') ?>" alt="Imagen de Proyecto"></div>
+        <div class="info">
+          <h3><?php the_field('titulo_proyecto_1') ?></h3>
+          <?php the_field('descripcion-proyecto_1') ?>
+        </div></a></div>
+      </div>
+      <div class="projectContainer truncate">
         <div class="ih-item square effect6 from_top_and_bottom"><a href="#">
-          <div class="img"><img src="<?php the_field('imagen-proyecto_1') ?>" alt="Imagen de Proyecto"></div>
+          <div class="img"><img src="<?php the_field('imagen-proyecto_2') ?>" alt="Imagen de Proyecto"></div>
           <div class="info">
-            <h3><?php the_field('titulo_proyecto_1') ?></h3>
-            <p><?php the_field('descripcion-proyecto_1') ?></p>
+            <h3><?php the_field('titulo_proyecto_2') ?></h3>
+            <?php the_field('descripcion-proyecto_2') ?>
           </div></a></div>
         </div>
-        <div class="projectContainer">
+        <div class="projectContainer truncate">
           <div class="ih-item square effect6 from_top_and_bottom"><a href="#">
-            <div class="img"><img src="<?php the_field('imagen-proyecto_2') ?>" alt="Imagen de Proyecto"></div>
+            <div class="img"><img src="<?php the_field('imagen-proyecto_3') ?>" alt="Imagen de Proyecto"></div>
             <div class="info">
-              <h3><?php the_field('titulo_proyecto_2') ?></h3>
-              <p><?php the_field('descripcion-proyecto_2') ?></p>
+              <h3><?php the_field('titulo_proyecto_3') ?></h3>
+              <?php the_field('descripcion-proyecto_3') ?>
             </div></a></div>
           </div>
-          <div class="projectContainer">
-            <div class="ih-item square effect6 from_top_and_bottom"><a href="#">
-              <div class="img"><img src="<?php the_field('imagen-proyecto_3') ?>" alt="Imagen de Proyecto"></div>
-              <div class="info">
-                <h3><?php the_field('titulo_proyecto_3') ?></h3>
-                <p><?php the_field('descripcion-proyecto_3') ?></p>
-              </div></a></div>
-            </div>
-          </div>
-          <div class="projectButton">
-            <a href="<?php echo get_bloginfo( 'url' ).'/proyectos'?>">Conoce nuestros proyectos</a>
-          </div>
-
-
         </div>
-      <?php endwhile ?>
+        <div class="projectButton">
+          <a href="<?php echo get_bloginfo( 'url' ).'/proyectos'?>">Conoce nuestros proyectos</a>
+        </div>
 
-      <?php get_footer(); ?>
+
+      </div>
+    <?php endwhile ?>
+
+    <?php get_footer(); ?>
