108dd253ca922b2426ab035d7362333ada36a660
Ariel Itzli Molina Sanchez <itzli2000@msn.com>
2017-07-19 10:54:36 -0500

    Added single.php to manage posts, working with posts styles

--- wp-content/themes/basetheme/index.php
+++ wp-content/themes/basetheme/index.php
@@ -3,7 +3,9 @@
 <?php while (have_posts()): the_post(); ?>
   <div id="mainContainer" class="posts">
     <div class="post_image">
-      <?php the_post_thumbnail(); ?>
+      <div class="cut_image">
+        <?php the_post_thumbnail(); ?>
+      </div>
     </div>
     <div class="author">
       <div class="date">
@@ -12,7 +14,7 @@
           <span class="month"><?php echo the_time('M'); ?></span>
         </time>
       </div>
-      <p>Publicado por: </p>
+      <span class="post_by">Publicado por: </span>
       <span><?php the_field('autor') ?></span>
     </div>
     <div class="post_cont">
