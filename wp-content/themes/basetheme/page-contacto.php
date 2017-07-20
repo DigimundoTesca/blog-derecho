af9c86ec34de2928a619f1b8a4d160f7817791a3
Itzli <itzli2000@msn.com>
2017-07-20 11:51:47 -0500

    Working with contact page

---
+++ wp-content/themes/basetheme/page-contacto.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+<?php get_template_part( 'banner' );  ?>
+<?php while (have_posts()): the_post(); ?>
+	<div id="mainContainer">
+		<div id="contactPage">
+			<?php the_content(); ?>
+		</div>
+		hola
+	</div>
+<?php endwhile ?>
+<?php get_footer(); ?>
