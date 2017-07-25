54756bc6e3eac49e40c1366c4d34475e1ad75fbd
Ariel Itzli Molina Sanchez <itzli2000@msn.com>
2017-07-25 10:45:28 -0500

    Working with contact page, added contact form 7 plugin and making moddifications to set fields

--- wp-content/themes/basetheme/functions.php
+++ wp-content/themes/basetheme/functions.php
@@ -11,9 +11,11 @@ add_action( 'after_setup_theme', 'theme_setup');
 function _adding_stylesheet(){
   //Style register
   wp_register_style('ihover',get_template_directory_uri().'/assets/css/ihover.css',array(),'1.0 ');
+  wp_register_style('card',get_template_directory_uri().'/assets/css/card.css',array(),'1.0 ');
   wp_register_style('style',get_template_directory_uri('ihover').'/style.css',array(),'1.0 ');
   //Style display
   wp_enqueue_style('ihover');
+  wp_enqueue_style('card');
   wp_enqueue_style('style');
 }
 add_action('wp_enqueue_scripts','_adding_stylesheet');
--- wp-content/themes/basetheme/page-contacto.php
+++ wp-content/themes/basetheme/page-contacto.php
@@ -5,6 +5,57 @@
 		<div id="contactPage">
 			<?php the_content(); ?>
 		</div>
-	</div>
-<?php endwhile ?>
-<?php get_footer(); ?>
+    <div id="cardContainer">
+      <!-- Card Publish-->
+      <div class="post-module">
+        <!-- Thumbnail-->
+        <div class="thumbnail">
+          <img src="<?php bloginfo('template_url'); ?>/assets//images/publish.jpg"/>
+        </div>
+        <!-- Post Content-->
+        <div class="post-content">
+          <div class="category">Artículo</div>
+          <h1 class="title">Para publicar un artículo</h1>
+          <h2 class="sub_title">Envia un archivo comprimido en formato "ZIP" conteniendo:</h2>
+          <p class="card-description">
+            Documento a publicar en formato .doc o .docx de Microsoft Office Word con el siguiente formato:<br>
+            ->Fuente: Georgia, tamaño 12 <br>
+            ->Interlineado: 1,5 <br>
+            ->Por lo menos una imagen contenida en el documento y por separado en formato png.<br>
+            Si no posee una imagem, háganoslo saber y conseguiremos una por usted.<br>
+            Curriculum e información de contacto.
+            No se aceptarán:<br>
+            ->Trabajos que no posean estructura o el formato solicitado.<br>
+            Su trabajo será verificado y publicado en cuanto se finalice su revisión.</p>
+          </div>
+        </div>
+
+        <!-- Job Publish-->
+        <div class="post-module">
+          <!-- Thumbnail-->
+          <div class="thumbnail">
+            <img src="<?php bloginfo('template_url'); ?>/assets//images/job.jpg"/>
+          </div>
+          <!-- Post Content-->
+          <div class="post-content">
+            <div class="category">Oferta de trabajo</div>
+            <h1 class="title">Para publicar una oferta</h1>
+            <h2 class="sub_title">Envia un archivo comprimido en formato "ZIP" conteniendo:</h2>
+            <p class="card-description">
+              Documento en formato .doc o .docx de Microsoft Office Word con el siguiente contenido:<br>
+              -> Nombre de puesto.<br>
+              -> Descripción completa del puesto.<br>
+              -> Nombre completo de la empresa.<br>
+              -> Página web de la empresa.<br>
+              -> Nombre completo de persona que postula el puesto..<br>
+              -> Curriculum e información de contacto.<br>
+              No se aceptarán:<br>
+              ->Trabajos que no posean estructura o el formato solicitado.<br>
+              Su trabajo será verificado y publicado en cuanto se finalice su revisión.</p>
+            </div>
+          </div>
+        </div>
+        <?php echo do_shortcode('[contact-form-7 id="122" title="Formulario de contacto"]'); ?>
+      </div>
+    <?php endwhile ?>
+    <?php get_footer(); ?>
