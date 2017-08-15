17990a29cdfb3a6ab96318985ee1897a0fd7394a
Ariel Itzli Molina Sanchez <itzli2000@msn.com>
2017-08-15 18:18:00 -0500

    Added post title on blog page

--- wp-content/themes/basetheme/index.php
+++ wp-content/themes/basetheme/index.php
@@ -4,35 +4,36 @@
   <div id="postsColumn">
     <?php while (have_posts()): the_post(); ?>
       <div id="eachPost">
-        <div class="post_image">
-          <div class="cut_image">
-            <?php the_post_thumbnail(); ?>
+        <h3><?php the_title(); ?></h4>
+          <div class="post_image">
+            <div class="cut_image">
+              <?php the_post_thumbnail(); ?>
+            </div>
+          </div>
+          <div class="author">
+            <div class="date">
+              <time >
+                <span class="day"><?php echo the_time('d'); ?></span>
+                <span class="month"><?php echo the_time('M'); ?></span>
+              </time>
+            </div>
+            <span class="post_by">Publicado por: </span>
+            <span><?php the_field('autor') ?></span>
+          </div>
+          <div class="post_cont">
+            <?php the_excerpt(); ?>
+            <a href="<?php the_permalink(); ?>" class="post_button">Leer más</a>
           </div>
         </div>
-        <div class="author">
-          <div class="date">
-            <time >
-              <span class="day"><?php echo the_time('d'); ?></span>
-              <span class="month"><?php echo the_time('M'); ?></span>
-            </time>
-          </div>
-          <span class="post_by">Publicado por: </span>
-          <span><?php the_field('autor') ?></span>
-        </div>
-        <div class="post_cont">
-          <?php the_excerpt(); ?>
-          <a href="<?php the_permalink(); ?>" class="post_button">Leer más</a>
-        </div>
-      </div>
-    <?php endwhile ?>
+      <?php endwhile ?>
+    </div>
+    <div id="widgetColumn">
+      <?php get_sidebar(); ?>
+    </div>
   </div>
-  <div id="widgetColumn">
-    <?php get_sidebar(); ?>
+  <div id="paginationContainer">
+    <div class="pagination">
+      <?php echo paginate_links(); ?>
+    </div>
   </div>
-</div>
-<div id="paginationContainer">
-  <div class="pagination">
-    <?php echo paginate_links(); ?>
-  </div>
-</div>
-<?php get_footer(); ?>  
+  <?php get_footer(); ?>  
